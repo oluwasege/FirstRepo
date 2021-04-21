f22e26e9a3a42795ea6a4da0903baa8c2f254aa7
Oluwasegun <akinpelu53@gmail.com>
2021-04-21 14:58:24 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
